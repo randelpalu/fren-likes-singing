d5c52c557b3e77f64d509934dded4797b14ff4f1
Randel Palu <randel.palu@yandex.com>
2024-03-19 23:17:26 +0200

    Added container gradient shadow style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Album from "./components/Album";
 
 export default function Home() {
   return (
-    <div className="container mx-auto mt-8 max-w-lg relative">
+    <div className="main-container container mx-auto mt-8 max-w-lg relative">
       <Album />
     </div>
   );
